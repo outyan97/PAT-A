9ec020adf2456ab8f0731bfa4ac0b3eb7d4e44ad
outyan97 <47840880+outyan97@users.noreply.github.com>
2020-03-01 00:51:13 +0800

    Add files via upload

---
+++ 1001.cpp
@@ -0,0 +1,24 @@
+#include "iostream"
+#include "string"
+#include "algorithm"
+
+int main() {
+    int a, b;
+    std::string sum;
+    std::cin >> a >> b;
+    sum = std::to_string(a + b);
+    int len = sum.length();
+    for (int i = 0; i < len; i++) {
+        if(sum[i] == '-') {
+            std::cout << sum[i];
+            continue;
+        }
+        std::cout << sum[i];
+        if((i + 1) % 3 == len % 3 && i != len - 1) {
+            std::cout << ',';
+        }
+    }
+    std::cout << std::endl;
+    system("pause");
+    return 0;
+}
